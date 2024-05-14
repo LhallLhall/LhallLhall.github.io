b73d5a89f96cbdb6e4688c802d59904163c1e649
Logan Hall <hall.logan@2819studios.com>
2024-05-13 23:53:24 -0400

    added about section with styling

--- portfolio/src/components/about.jsx
+++ portfolio/src/components/about.jsx
@@ -1,9 +1,128 @@
+import React from "react";
+import styles from "../styles/about.module.css"
+
 export default function About() {
-    return (
-    <div className="h-screen">
-      <div className="container mx-auto h-screen ">
-        <p>test</p>
+  return (
+    <div className='h-screen'>
+      <div className='container mx-auto '>
+        <div className={styles.codeWrp}>
+          <div data-line-nr='01' className={styles.codel}>
+            <div className={styles.methods}>class</div>
+            <span className={styles.propname}> Logan Hall</span> &#123;
+          </div>
+          <div data-line-nr='02' className={styles.codel}>
+            <div className={styles.dot}>··</div>
+            <div className={styles.comment}>// I think, therfore I am</div>
+          </div>
+          <div data-line-nr='03' className={styles.codel}>
+            <div className={styles.dot}>··</div>
+            <div className={styles.comment}>// My vast variety of skills is continuously expanding.</div>
+          </div>
+          <div data-line-nr='04' className={styles.codel}>
+            <div className={styles.dot}>··</div>
+            <div className={styles.methods}>constructor</div>() &#123;
+          </div>
+          <div data-line-nr='05' className={styles.codel}>
+            <div className={styles.dot}>····</div>
+            <span className={styles.scope}>this</span>.<span className='prop2'>name</span> <span className={styles.scope}>=</span>{" "}
+            <span className={styles.string}>'Logan Hall'</span>
+          </div>
+          <div data-line-nr='06' className={styles.codel}>
+            <div className={styles.dot}>····</div>
+            <span className={styles.scope}>this</span>.<span className='prop2'>dayOfBirthTimestamp</span>{" "}
+            <span className={styles.scope}>=</span> <span className='number'>1072819200</span>
+          </div>
+          <div data-line-nr='07' className={styles.codel}>
+            <div className={styles.dot}>····</div>
+            <span className={styles.scope}>this</span>.<span className='prop2'>email</span> <span className={styles.scope}>=</span>{" "}
+            <span className={styles.string}>'hall.logan@2819studios.com'</span>
+          </div>
+          <div data-line-nr='08' className={styles.codel}>
+            <div className={styles.dot}>··</div>&#125;
+          </div>
+          <div data-line-nr='09' className={styles.codel}>
+            <div className={styles.dot}>··</div>
+            <span className={styles.propname}>workExperience</span>() &#123;
+          </div>
+          <div data-line-nr='10' className={styles.codel}>
+            <div className={styles.dot}>····</div>
+            <div className={styles.methods}>return</div> [
+          </div>
+          <div data-line-nr='11' className={styles.codel}>
+            <div className={styles.dot}>······</div>
+            &#123; <span className={styles.string}>'2020-now'</span> :
+            <span className={styles.string}> 'Full-stack Developer/Owner at Pixel Lab'</span> &#125;
+          </div>
+          <div data-line-nr='12' className={styles.codel}>
+            <div className={styles.dot}>······</div>
+            &#123; <span className={styles.string}>'2023-now'</span> :
+            <span className={styles.string}> 'Solutions Architect at Digitree Group S.A.'</span> &#125;
+          </div>
+          <div data-line-nr='13' className={styles.codel}>
+            <div className={styles.dot}>······</div>
+            &#123; <span className={styles.string}>'2017-2023'</span> :
+            <span className={styles.string}> 'Full-stack Developer at Digitree Group S.A.'</span> &#125;
+          </div>
+          <div data-line-nr='14' className={styles.codel}>
+            <div className={styles.dot}>······</div>
+            &#123; <span className={styles.string}>'2013-2017'</span>:
+            <span className={styles.string}> 'Full-Stack Designer at Digitree Group S.A.'</span> &#125;
+          </div>
+          <div data-line-nr='15' className={styles.codel}>
+            <div className={styles.dot}>······</div>
+            &#123; <span className={styles.string}>'2006-now'</span> :
+            <span className={styles.string}> 'Full-Stack Designer/developer at Yasio.dev'</span> &#125;
+          </div>
+          <div data-line-nr='16' className={styles.codel}>
+            <div className={styles.dot}>····</div>]
+          </div>
+          <div data-line-nr='17' className={styles.codel}>
+            <div className={styles.dot}>··</div>&#125;
+          </div>
+          <div data-line-nr='18' className={styles.codel}>
+            <div className={styles.dot}>··</div>
+            <span className={styles.propname}>education</span>() &#123;
+          </div>
+          <div data-line-nr='19' className={styles.codel}>
+            <div className={styles.dot}>····</div>
+            <div className={styles.methods}>return</div> [
+          </div>
+          <div data-line-nr='20' className={styles.codel}>
+            <div className={styles.dot}>······</div>
+            &#123; <span className={styles.string}>'2022'</span>:
+            <span className={styles.string}>
+              {" "}
+              'Awesome Inc Web Development Bootcamp - A 16 week program designed to teach you HTML/CSS/JS, React, Python, Django, and Bootstrap. Made over 13 different projects using weekly agile sprints.'
+            </span>{" "}
+            &#125;,
+          </div>
+          <div data-line-nr='22' className={styles.codel}>
+            <div className={styles.dot}>····</div>]
+          </div>
+          <div data-line-nr='23' className={styles.codel}>
+            <div className={styles.dot}>··</div>&#125;
+          </div>
+          <div data-line-nr='24' className={styles.codel}>
+            <div className={styles.dot}>··</div>
+            <span className={styles.propname}>skills</span>() &#123;
+          </div>
+          <div data-line-nr='25' className={styles.codel}>
+            <div className={styles.dot}>····</div>
+            <div className={styles.methods}>return</div> [{" "}
+            <span className={styles.string}>
+              'HTML/CSS/JS', 'Node.js', 'Bootstrap/Tailwind', 'Vite', 'npm/yarn/pnpm', 'Docker', 'Firebase', 'PHP', 'Laravel',
+              'ORM', 'Alembic/FastAPI', 'Python', 'SQL', 'Django', 'PostgressSQL' 'Wordpress', 'UX/UI', 'AWS', 'C#', 'TypeScript', 'NestJS', 'Postman', 'Thunderclient', 'GA4/Google Search Console', 'Agile Development', 'OOP', 'Responsive Design'  {" "}
+            </span>{" "}
+            <span className='cursor'></span> ]
+          </div>
+          <div data-line-nr='26' className={styles.codel}>
+            <div className={styles.dot}>··</div>&#125;
+          </div>
+          <div data-line-nr='27' className={styles.codel}>
+            &#125;
+          </div>
+        </div>
       </div>
     </div>
-    )
+  );
 }
--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -24,7 +24,7 @@ export default function NavComponent() {
             <span>Projects</span>
             <span className="text-violet-600">/&gt;</span>
           </Navbar.Link>
-          <Navbar.Link className="md:hover:text-gray-700 border-none my-5 me-0 md:me-4 relative inline cursor-pointer text-white text-xl font-medium before:bg-violet-600  before:absolute before:-bottom-1 before:block before:h-[2px] before:w-full before:origin-bottom-right before:scale-x-0 before:transition before:duration-300 before:ease-in-out hover:before:origin-bottom-left hover:before:scale-x-100" as={Link} href="#">
+          <Navbar.Link className="md:hover:text-gray-700 border-none my-5 me-0 md:me-4 relative inline cursor-pointer text-white text-xl font-medium before:bg-violet-600  before:absolute before:-bottom-1 before:block before:h-[2px] before:w-full before:origin-bottom-right before:scale-x-0 before:transition before:duration-300 before:ease-in-out hover:before:origin-bottom-left hover:before:scale-x-100" as={Link} href="#about">
             <span className="text-violet-600">&lt;</span>
             <span>About</span>
             <span className="text-violet-600">/&gt;</span>
